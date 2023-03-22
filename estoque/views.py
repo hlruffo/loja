d029e80e892749a515a7250f5576c589471056d3
Hugo L. Ruffo <hlruffo@gmail.com>
2023-03-22 09:15:13 -0300

    Fim aula 4

--- estoque/views.py
+++ estoque/views.py
@@ -1,7 +1,12 @@
+from datetime import date
+from io import BytesIO
+
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.http import HttpResponse
 from django.shortcuts import render
-
-from .models import Categoria
+from PIL import Image, ImageDraw
+import sys
+from .models import Categoria, Imagem, Produto
 
 
 # Create your views here.
@@ -15,7 +20,42 @@ def add_produto(request):
         quantidade = request.POST.get('quantidade')
         preco_compra = request.POST.get('preco_compra')
         preco_venda = request.POST.get('preco_venda')
-        imagens = request.FILES
+        imagens = request.FILES.getlist('imagens')
 
-        print(imagens)
+        produto = Produto(
+            nome=nome,
+            categoria_id=categoria,
+            quantidade=quantidade,
+            preco_compra=preco_compra,
+            preco_venda=preco_venda
+        )
+        produto.save()
+
+        for f in request.FILES.getlist('imagens'):
+            name = f'{date.today()}-{produto.id}.jpg'
+
+            img = Image.open(f)
+            img = img.convert('RGB')
+            img = img.resize((300, 300))
+            draw = ImageDraw.Draw(img)
+            draw.text(
+                (20, 280), f'Propriedade de Loja em {date.today()}', (255, 255, 255))
+
+            output = BytesIO()  # salvar o arquivo configurado em bytes -> não salva na memoria
+            img.save(output, format="JPEG", quality=100)
+            # retorna o ponteiro para o começo do arquivo da imagem NECESSARIO
+            output.seek(0)
+            img_final = InMemoryUploadedFile(  # converte usando o InMemoryUploadedFiel
+                        output,
+                        'ImageField',
+                        name,
+                        'image/jpeg',
+                        sys.getsizeof(output),
+                        None
+            )
+            img_dj = Imagem(
+                imagem=img_final,
+                produto=produto
+            )
+            img_dj.save()
         return HttpResponse("OK")
